f4dfda4a71077f033a12e4f38ecb5996077eba44
tom-jr <tomjavastdy@gmail.com>
2022-03-03 05:43:37 -0300

    #001 -create projec
     Criaçao do projeto com spring initialize, e comandos mvn

---
+++ algafood-api/src/main/java/com/tom/algafoodapi/AlgafoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.tom.algafoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodApiApplication.class, args);
+	}
+
+}
